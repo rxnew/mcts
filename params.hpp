f264762f18183d0478304aea738d34f5a2ad4df5
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2016-04-11 02:18:58 +0900

    init

---
+++ params.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace mcts {
+extern int playout_number;
+extern int playout_limit;
+extern double evaluation_param;
+}
---
+++ state.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <list>
+
+namespace mcts {
+struct AbstractState {
+  AbstractState() = default;
+  AbstractState(const AbstractState&) = default;
+  AbstractState(AbstractState&&) noexcept = default;
+  virtual ~AbstractState() = default;
+  auto operator=(const AbstractState&) -> AbstractState& = default;
+  auto operator=(AbstractState&&) -> AbstractState& = default;
+  // Please override the following functions
+  template <class State>
+  auto getNextStates() const -> std::list<State>;
+  virtual auto isEnd() const -> bool = 0;
+  virtual auto shiftNextRandomly() -> void = 0;
+  virtual auto getScore() const -> int = 0;
+};
+}
