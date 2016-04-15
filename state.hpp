91b54faacaaedd0574ef80dc63f8075883e7d857
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2016-04-15 14:13:31 +0900

    virtual化テスト

--- state.hpp
+++ state.hpp
@@ -18,7 +18,7 @@ struct AbstractState {
   auto operator=(AbstractState&&) -> AbstractState& = default;
 
   // Please override the following functions
-  auto getNextStates() const -> std::list<typename decltype(std::remove_reference<*this>::type)>;
+  auto getNextStates() const -> std::list<typename std::remove_reference<decltype(*this)>::type>>;
   virtual auto isEnd() const -> bool = 0;
   virtual auto shiftNextRandomly() -> void = 0;
   virtual auto getScore() const -> int = 0;
